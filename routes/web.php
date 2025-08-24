b4a1b87540951adb354b10679c3f1d1873280fe7
Jann Mari Poblador <jannmaripoblador@gmail.com>
2025-08-24 18:11:37 +0800

    Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
-Route::prefix('users')->name('users.')->group(function () {
-    Route::get('/create', [UserController::class, 'create'])->name('create');
-    Route::post('/', [UserController::class, 'store'])->name('store');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-// Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-// Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::resource('users', UserController::class);
+Route::get('/test', function () {
+    return view('test');
+})->middleware(['auth', 'verified'])->name('test');
 
-// Route::get('/users/create', function () {
-//     return view('components.users.create');
-// });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
